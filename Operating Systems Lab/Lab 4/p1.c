125e93bb088ab42609ecaeedb50e968e8ad116df
Avinash Changrani <74554800+nabobery@users.noreply.github.com>
2022-08-26 16:27:37 +0530

    Update OS Lab4 p1.c

--- Operating Systems Lab/Lab 4/p1.c
+++ Operating Systems Lab/Lab 4/p1.c
@@ -18,62 +18,23 @@ int compare(const void *a, const void *b)
     return (p->processId - q->processId);
 }
 
-void updateQueue(int queue[], int timer, process processes[], int n, int maxProccessIndex)
+int comparepid(const void *a, const void *b)
 {
-    int zeroIndex;
-    for (int i = 0; i < n; i++)
-    {
-        if (queue[i] == 0)
-        {
-            zeroIndex = i;
-            break;
-        }
-    }
-    queue[zeroIndex] = maxProccessIndex + 1;
-}
-
-void maintainQueue(int queue[], int n)
-{
-    for (int i = 0; (i < n - 1) && (queue[i + 1] != 0); i++)
-    {
-        int temp = queue[i];
-        queue[i] = queue[i + 1];
-        queue[i + 1] = temp;
-    }
-}
-
-void checkNewArrival(int timer, process processes[], int n, int maxProccessIndex, int queue[])
-{
-    if (timer <= processes[n - 1].arrivalTime)
-    {
-        bool newArrival = false;
-        for (int j = (maxProccessIndex + 1); j < n; j++)
-        {
-            if (processes[j].arrivalTime <= timer)
-            {
-                if (maxProccessIndex < j)
-                {
-                    maxProccessIndex = j;
-                    newArrival = true;
-                }
-            }
-        }
-        // adding to ready queue
-        if (newArrival)
-            updateQueue(queue, timer, processes, n, maxProccessIndex);
-    }
+    process *p = (process *)a;
+    process *q = (process *)b;
+    return (p->processId - q->processId);
 }
 
 int main()
 {
-    int numberOfProcesses, timeQuantum, timer = 0, maxProccessIndex = 0;
+    int numberOfProcesses, timeQuantum, timer = 0, maxProccessIndex = 0, contextSwitchingOverHead = 0, completed = 0, numberOfContextSwitches = 0;
     double averageWaitingTime = 0, averageTurnAroundTime = 0;
     printf("Please enter the number of Processes: \n");
     scanf("%d", &numberOfProcesses);
     process processes[numberOfProcesses];
     char choice;
-    printf("(a)All arriving at Time 0\n(b)All arrive at different time\nPlease Enter your choice: \n");
-    scanf(" %c", &choice);
+    // printf("(a)All arriving at Time 0\n(b)All arrive at different time\nPlease Enter your choice: \n");
+    // scanf(" %c", &choice);
     int tempBurstTime[numberOfProcesses], queue[numberOfProcesses];
     for (int i = 0; i < numberOfProcesses; i++)
     {
@@ -83,168 +44,97 @@ int main()
     }
     printf("Please enter the Time Quantum: \n");
     scanf("%d", &timeQuantum);
+    printf("Please enter the Context Context Switching OverHead: \n");
+    scanf("%d", &contextSwitchingOverHead);
     qsort((void *)processes, numberOfProcesses, sizeof(processes[0]), compare);
-    bool completed[numberOfProcesses];
+    process readyQueue[1000];
+    int front = 0, rear = 0, arrived = 0, index[1000];
+    while (completed < numberOfProcesses)
+    {
+        for (timer = processes[arrived].arrivalTime; arrived < numberOfProcesses; arrived++)
+        {
+            if (processes[arrived].arrivalTime == timer)
+            {
+                readyQueue[rear] = processes[arrived];
+                index[rear] = arrived;
+                rear++;
+            }
+            else
+                break;
+        }
+        while (front < rear)
+        {
+            if (readyQueue[front].burstTime <= timeQuantum)
+            {
+                for (timer += readyQueue[front].burstTime; arrived < numberOfProcesses; arrived++)
+                {
+                    if (processes[arrived].arrivalTime <= timer)
+                    {
+                        readyQueue[rear] = processes[arrived];
+                        index[rear] = arrived;
+                        rear++;
+                    }
+                    else
+                        break;
+                }
+                processes[index[front]].completionTime = timer;
+                completed++;
+            }
+            else
+            {
+                for (timer += timeQuantum; arrived < numberOfProcesses; arrived++)
+                {
+                    if (processes[arrived].arrivalTime <= timer)
+                    {
+                        readyQueue[rear] = processes[arrived];
+                        index[rear] = arrived;
+                        rear++;
+                    }
+                    else
+                        break;
+                }
+                readyQueue[front].burstTime -= timeQuantum;
+                readyQueue[rear] = readyQueue[front];
+                rear++;
+            }
+            front++;
+            if (front < rear)
+            {
+                if (readyQueue[front].processId != readyQueue[front - 1].processId)
+                {
+                    for (timer += contextSwitchingOverHead; arrived < numberOfProcesses; arrived++)
+                    {
+                        if (processes[arrived].arrivalTime <= timer)
+                        {
+                            readyQueue[rear] = processes[arrived];
+                            index[rear] = arrived;
+                            rear++;
+                        }
+                        else
+                            break;
+                    }
+                    // numberOfContextSwitches++;
+                }
+            }
+        }
+    }
     for (int i = 0; i < numberOfProcesses; i++)
     {
-        completed[i] = false;
-        queue[i] = 0;
+        processes[i].turnAroundTime = (processes[i].completionTime - processes[i].arrivalTime);
+        processes[i].waitingTime = (processes[i].turnAroundTime - processes[i].burstTime);
+        averageTurnAroundTime += processes[i].turnAroundTime;
+        averageWaitingTime += processes[i].waitingTime;
     }
-    if (choice == 'a')
+    averageTurnAroundTime /= numberOfProcesses;
+    averageWaitingTime /= numberOfProcesses;
+    qsort((void *)processes, numberOfProcesses, sizeof(processes[0]), comparepid);
+    printf("Process ID \t Arrival Time \t Burst Time \t Completion Time \t Waiting Time \t Turn Around Time \n");
+    for (int i = 0; i < numberOfProcesses; i++)
     {
-        while (timer < processes[0].arrivalTime)
-            timer++;
-        queue[0] = 1;
-        while (true)
-        {
-            bool flag = true;
-            for (int i = 0; i < numberOfProcesses; i++)
-            {
-                if (tempBurstTime[i] != 0)
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag)
-                break;
-            for (int i = 0; (i < numberOfProcesses) && (queue[i] != 0); i++)
-            {
-                int counter = 0;
-                while ((counter < timeQuantum) && (tempBurstTime[queue[0] - 1] > 0))
-                {
-                    tempBurstTime[queue[0] - 1] -= 1;
-                    timer += 1;
-                    counter++;
-                    // Checking & updating the ready queue until time=timer
-                    checkNewArrival(timer, processes, numberOfProcesses, maxProccessIndex, queue);
-                }
-                // If a process is completed, mark completed as true and store completion time
-                if ((tempBurstTime[queue[0] - 1] == 0) && (completed[queue[0] - 1] == false))
-                {
-                    processes[queue[0] - 1].completionTime = timer;
-                    completed[queue[0] - 1] = true;
-                }
-                // checks whether or not CPU is idle
-                bool idle = true;
-                if (queue[numberOfProcesses - 1] == 0)
-                {
-                    for (int i = 0; i < numberOfProcesses && queue[i] != 0; i++)
-                    {
-                        if (completed[queue[i] - 1] == false)
-                        {
-                            idle = false;
-                        }
-                    }
-                }
-                else
-                    idle = false;
-                if (idle)
-                {
-                    timer++;
-                    checkNewArrival(timer, processes, numberOfProcesses, maxProccessIndex, queue);
-                }
-                // Maintaining the entries of processes after each premption in the ready Queue
-                maintainQueue(queue, numberOfProcesses);
-            }
-        }
-        for (int i = 0; i < numberOfProcesses; i++)
-        {
-            processes[i].turnAroundTime = (processes[i].completionTime - processes[i].arrivalTime);
-            processes[i].waitingTime = (processes[i].turnAroundTime - processes[i].burstTime);
-            averageTurnAroundTime += processes[i].turnAroundTime;
-            averageWaitingTime += processes[i].waitingTime;
-        }
-        averageTurnAroundTime /= numberOfProcesses;
-        averageWaitingTime /= numberOfProcesses;
-        printf("Process ID \t Arrival Time \t Burst Time \t Completion Time \t Waiting Time \t Turn Around Time \n");
-        for (int i = 0; i < numberOfProcesses; i++)
-        {
-            printf("%d \t %d \t %d \t %d \t %d \t %d \n", processes[i].processId, processes[i].arrivalTime, processes[i].burstTime, processes[i].completionTime, processes[i].waitingTime, processes[i].turnAroundTime);
-        }
-        printf("The average Waiting Time is %lf \n", averageWaitingTime);
-        printf("The average Turn Around Time is %lf \n", averageTurnAroundTime);
+        printf("%d \t %d \t %d \t %d \t %d \t %d \n", processes[i].processId, processes[i].arrivalTime, processes[i].burstTime, processes[i].completionTime, processes[i].waitingTime, processes[i].turnAroundTime);
     }
-    else if (choice == 'b')
-    {
-        while (timer < processes[0].arrivalTime)
-            timer++;
-        queue[0] = 1;
-        while (true)
-        {
-            bool flag = true;
-            for (int i = 0; i < numberOfProcesses; i++)
-            {
-                if (tempBurstTime[i] != 0)
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag)
-                break;
-
-            for (int i = 0; (i < numberOfProcesses) && (queue[i] != 0); i++)
-            {
-                int counter = 0;
-                while ((counter < timeQuantum) && (tempBurstTime[queue[0] - 1] > 0))
-                {
-                    tempBurstTime[queue[0] - 1] -= 1;
-                    timer += 1;
-                    counter++;
-                    // Checking & updating the ready queue until time=timer
-                    checkNewArrival(timer, processes, numberOfProcesses, maxProccessIndex, queue);
-                }
-                // If a process is completed, mark completed as true and store completion time
-                if ((tempBurstTime[queue[0] - 1] == 0) && (completed[queue[0] - 1] == false))
-                {
-                    processes[queue[0] - 1].completionTime = timer;
-                    completed[queue[0] - 1] = true;
-                }
-                // checks whether or not CPU is idle
-                bool idle = true;
-                if (queue[numberOfProcesses - 1] == 0)
-                {
-                    for (int i = 0; i < numberOfProcesses && queue[i] != 0; i++)
-                    {
-                        if (completed[queue[i] - 1] == false)
-                        {
-                            idle = false;
-                        }
-                    }
-                }
-                else
-                    idle = false;
-                if (idle)
-                {
-                    timer++;
-                    checkNewArrival(timer, processes, numberOfProcesses, maxProccessIndex, queue);
-                }
-                // Maintaining the entries of processes after each premption in the ready Queue
-                maintainQueue(queue, numberOfProcesses);
-            }
-        }
-        for (int i = 0; i < numberOfProcesses; i++)
-        {
-            processes[i].turnAroundTime = (processes[i].completionTime - processes[i].arrivalTime);
-            processes[i].waitingTime = (processes[i].turnAroundTime - processes[i].burstTime);
-            averageTurnAroundTime += processes[i].turnAroundTime;
-            averageWaitingTime += processes[i].waitingTime;
-        }
-        averageTurnAroundTime /= numberOfProcesses;
-        averageWaitingTime /= numberOfProcesses;
-        printf("Process ID \t Arrival Time \t Burst Time \t Completion Time \t Waiting Time \t Turn Around Time \n");
-        for (int i = 0; i < numberOfProcesses; i++)
-        {
-            printf("%d \t %d \t %d \t %d \t %d \t %d \n", processes[i].processId, processes[i].arrivalTime, processes[i].burstTime, processes[i].completionTime, processes[i].waitingTime, processes[i].turnAroundTime);
-        }
-        printf("The average Waiting Time is %lf \n", averageWaitingTime);
-        printf("The average Turn Around Time is %lf \n", averageTurnAroundTime);
-    }
-    else
-    {
-        printf("You've entered the wrong choice. Try again\n");
-    }
+    printf("The average Waiting Time is %lf \n", averageWaitingTime);
+    printf("The average Turn Around Time is %lf \n", averageTurnAroundTime);
+    // printf("The number of Context Switches is %d \n", numberOfContextSwitches);
     return 0;
 }
-
